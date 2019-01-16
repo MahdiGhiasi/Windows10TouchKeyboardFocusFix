61f6627764252450ac5170a20b7a97d4b6e1f095
Mahdi Ghiasi <mghiasi75@gmail.com>
2019-01-16 17:18:56 +0330

    Make app single instance

--- Windows10TouchKeyboardFocusFix/Program.cs
+++ Windows10TouchKeyboardFocusFix/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,16 +10,33 @@ namespace Windows10TouchKeyboardFocusFix
 {
     static class Program
     {
+        // Mutex can be made static so that GC doesn't recycle
+        // same effect with GC.KeepAlive(mutex) at the end of main
+        static Mutex mutex = new Mutex(false, "Windows10TouchKeyboardFocusFix");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            SquirrelHelper.ProcessSquirrelEvents();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DockForm());
+            if (!mutex.WaitOne(TimeSpan.FromSeconds(1), false))
+            {
+                Debug.WriteLine("Already running. This instance will exit.");
+                return;
+            }
+
+            try
+            {
+                SquirrelHelper.ProcessSquirrelEvents();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new DockForm());
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
         }
     }
 }
