02403662b5b04fceb7097369c2e729063011c6cc
Mahdi Ghiasi <mghiasi75@gmail.com>
2019-01-16 13:35:39 +0330

    Modified assembly title

--- Windows10TouchKeyboardFocusFix/Properties/AssemblyInfo.cs
+++ Windows10TouchKeyboardFocusFix/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Touch Keyboard Textbox Focus Fix for Windows 10")]
+[assembly: AssemblyTitle("Touch Keyboard Focus Fix for Windows 10")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
